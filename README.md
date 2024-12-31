b2ac915363566bbc29c5642dcefdbd2472094643
Bagas Septian <bagaseptian26@gmail.com>
2024-12-31 04:10:12 +0000

    request '/api/whoami' return JSON object with ipaddress in the ipaddress key,request '/api/whoami' return JSON object with language in the language key,request '/api/whoami' return JSON object with software in the software key

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Request Header Parser Microservice
 
 This is the boilerplate for the Request Header Parser Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/request-header-parser-microservice
+
+Melakukan stamp untuk melihat 
+("ipaddress),
+("language"),
+("software")
+dengan metode Request Header Parser Microservice
--- index.js
+++ index.js
@@ -24,6 +24,25 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//end point /api/whoami
+app.get("/api/whoami",function(req,res){
+  //Mendapatkan IP nya dari header x-forward
+  const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  //mendapat preffered language dari header 'acc lang'
+  const language = req.headers['accept-language'];
+
+  //mendapat informasi software user-agent dari header user-agent
+  const software = req.headers['user-agent'];
+
+  //kirim response JSON
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
